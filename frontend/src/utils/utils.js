507d46f5e3bd18021299b87639a2cfdb6ae8d8a6
Gaurav <138099351+koligaurav@users.noreply.github.com>
2025-04-03 12:41:12 +0530

    Update utils.js

--- frontend/src/utils/utils.js
+++ frontend/src/utils/utils.js
@@ -1,1 +1,1 @@
-export const BACKEND_URL = "https://course-selling-website-soc-1.onrender.com/api/v1";
+export const BACKEND_URL = "https://course-selling-website-soc-1.onrender.com/course/courses";
